942533eb8daffdd285679e2cc7a84a95a2c0f105
m4rkbello <markbello10041999@gmail.com>
2025-06-18 19:44:44 +0800

    OHAHAY!

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
+import React, { JSX } from 'react';
 import { View, Text } from 'react-native';
 
 
-const App = () => {
+function App(): JSX.Element {
   return (
     <View>
       <Text style={{ fontSize: 30 }}>ULOL HAHAHA!!!</Text>
     </View>
-  )
+  );
 }
 
 export default App;
