abf94317f90a147e359044c4ef47331ca12f912d
m4rkbello <markbello10041999@gmail.com>
2025-06-19 11:20:08 +0800

    STATE AND PROPS WITH BUTTON OHAHAY!

--- components/PropsComponent.js
+++ components/PropsComponent.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Button, Text } from 'react-native';
 
 const PropsParentComponent = () => {
 
-    let name = "MARKBELLO";
+    let name = "PROPS DRILLING PARENT TO CHILD!";
+    const [alias, setAlias] = useState("MARK");
 
     return (
         <View>
             <Text style={{ fontSize: 30 }}>PROPS PARENT/CHILD </Text>
             <PropsChildrenComponent name={name} />
-
+            <PropsChildrenComponent alias={alias} />
+            <Button title="PROPS EVENTS!" onPress={() => setAlias("OHAHAY!")} />
         </View>
     );
 };
 
 
-const PropsChildrenComponent = (ohahay) => {
+const PropsChildrenComponent = (x) => {
+    console.log(x);
     return (
         <View>
-            <Text style={{ fontSize: 30 }}>PROPS TO CHILD! {ohahay.name} </Text>
+            <Text style={{ fontSize: 30 }}>PROPS TO CHILD COMPONENT! {x.name} </Text>
+            <Text style={{ fontSize: 30 }}>PROPS TO CHILD COMPONENT! WITH EVENT ONPRESS {x.alias} </Text>
+
         </View>
     )
 }
